1f7c456c7ac0bd048d44348c2613fa52278df2ce
fauzan127 <fauzan.rosyad0491@student.unri.ac.id>
2025-11-18 22:52:19 +0700

    Ubah penggunaan csrf di login dan register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
-        @csrf
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Email Address -->
         <div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,7 @@
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
-        @csrf
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
         <!-- Name -->
         <div>
