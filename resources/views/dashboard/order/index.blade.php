146861a64dd6b8adaf2a8385070540c93ee46c9a
abdul-rahman-aziz <abdul.rahman2079@student.unri.ac.id>
2025-11-26 15:18:42 +0700

    ubah icon aksi order

--- resources/views/dashboard/order/index.blade.php
+++ resources/views/dashboard/order/index.blade.php
@@ -87,12 +87,23 @@
                                     {{ ucfirst(str_replace('_', ' ', $order->payment_status ?? '-')) }}
                                 </span>
                             </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
-                                <a href="{{ route('order.edit', $order->id) }}" class="text-indigo-600 hover:text-indigo-900 mx-1">Edit</a>
-                                <form action="{{ route('order.destroy', $order->id) }}" method="POST" class="inline" onsubmit="return confirm('Apakah Anda yakin ingin menghapus order ini?');">
+                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex items-center justify-end space-x-3">
+                                {{-- Tombol Edit (Ikon Pensil) --}}
+                                <a href="{{ route('order.edit', $order->id) }}" title="Edit Pesanan"
+                                   class="text-blue-600 hover:text-blue-800 flex items-center">
+                                    {{-- SVG Icon Pensil --}}
+                                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
+                                </a>
+
+                                {{-- Tombol Hapus (Ikon Tempat Sampah) --}}
+                                <form action="{{ route('order.destroy', $order->id) }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin menghapus pesanan ini? Aksi ini tidak dapat dibatalkan.');" class="m-0 p-0">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="text-red-600 hover:text-red-900 mx-1">Hapus</button>
+                                    <button type="submit" title="Hapus Pesanan"
+                                            class="text-red-600 hover:text-red-800 flex items-center">
+                                        {{-- SVG Icon Tempat Sampah (Trash) --}}
+                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+                                    </button>
                                 </form>
                             </td>
                         @endif
